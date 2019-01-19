a6283b6d748c481deed30c34338637d286292d2b
SteMMo33 <stefano.mora@libero.it>
2019-01-19 16:03:17 +0100

    Corretto problema enumerazione *ngFor

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { JokeComponent } from './joke/joke';
 import { JokeListComponent } from './joke-list/joke-list';
+import { CommonModule } from '@angular/common';
+
 @NgModule({
 	declarations: [JokeComponent,
     JokeListComponent],
-	imports: [],
+	imports: [ CommonModule],
 	exports: [JokeComponent,
     JokeListComponent]
 })
